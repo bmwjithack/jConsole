1da48c2abc0c26a554770a68572af9d87e9dd335
Brandon Lewis <bmlewis357@yahoo.com>
2018-06-26 21:36:11 -0400

    Added science

--- src/main/java/greeting/HelloController.java
+++ src/main/java/greeting/HelloController.java
@@ -1,7 +1,13 @@
 package greeting;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +21,13 @@ public class HelloController {
     @RequestMapping("/greeting")
     public Hello greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Hello(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+    @RequestMapping("/jscience")
+    String hello(Map<String, Object> model) {
+        RelativisticModel.select();
+        Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+        model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+        return "hello";
     }
 }
